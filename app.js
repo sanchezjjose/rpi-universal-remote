86ed8cdb50029f53a75f86fa99e18a9259315240
Jose Sanchez <sanchezjjose@gmail.com>
2017-09-09 19:18:26 -0400

    basic on off commands

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const lircNode = require('lirc_node')
 const app = express()
 
+lirc_node = require('lirc_node');
+lirc_node.init();
+
+// To see all of the remotes and commands that LIRC knows about:
+console.log(lirc_node.remotes);
+
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+
+    lirc_node.irsend.send_once("fujitsu_heat_ac", "cool-on", function() {
+      console.log("Sent AC cool power command.");
+    });
+
+    res.send('AC is ON!')
+})
+
+app.get('/off', function (req, res) {
+
+    lirc_node.irsend.send_once("fujitsu_heat_ac", "turn-off", function() {
+      console.log("Sent AC power off command.");
+    });
+
+    res.send('AC is OFF!')
 })
 
 app.listen(3000, function () {
