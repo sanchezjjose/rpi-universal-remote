ecd1c45e36abb20907074b3d14ae2b6b91e2e758
josanche <jose.sanchez@mlb.com>
2017-09-11 22:49:34 -0400

    cleaning up routes

--- app.js
+++ app.js
@@ -9,27 +9,25 @@ lirc_node.init();
 console.log(lirc_node.remotes);
 
 app.get('/', function (req, res) {
-    res.send('Welcome to your home universal remote controller')
+    console.log(req.query);
+    res.send('Welcome to your home universal remote controller');
 })
 
 app.get('/on', function (req, res) {
-
     lirc_node.irsend.send_once("fujitsu_heat_ac", "cool-on", function() {
-      console.log("Sent AC cool power command.");
+        console.log("Sent AC cool power command.");
+        res.send('AC is ON!');
     });
-
-    res.send('AC is ON!')
-})
+});
 
 app.get('/off', function (req, res) {
-
     lirc_node.irsend.send_once("fujitsu_heat_ac", "turn-off", function() {
-      console.log("Sent AC power off command.");
+        console.log("Sent AC power off command.");
+        res.send('AC is OFF!');
     });
-
-    res.send('AC is OFF!')
-})
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port 3000!');
+});
+
