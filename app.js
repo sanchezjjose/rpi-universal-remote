8584512dced040582c611baf2220697cf8dc06ca
josanche <jose.sanchez@mlb.com>
2017-09-16 00:51:12 -0400

    updating status endpoint

--- app.js
+++ app.js
@@ -7,46 +7,45 @@ let currentStatus = 'unknown';
 lirc_node = require('lirc_node');
 lirc_node.init();
 
-// To see all of the remotes and commands that LIRC knows about:
-console.log(lirc_node.remotes);
-
 function sendCommand (command, callback) {
     console.log(`Sending command ${command}`);
+    currentStatus = command;
+
     lirc_node.irsend.send_once('fujitsu_heat_ac', command, callback);
 };
 
 app.get('/', (req, res) => {
-    console.log(req.query);
     res.send('Welcome to your home universal remote controller.');
 });
 
 app.get('/status', (req, res) => {
+    console.log('Getting current status.');
     res.send(currentStatus);
 });
 
 // TODO: split into 2 routes -- /alexa/api/v1/on and /on
 app.get('/on', (req, res) => {
-    console.log(req.query);
+    console.log('Sending power on command: ', req.query);
 
     const mode = req.query.mode || 'dry'; // cool, heat, dry, fan
     const fanSpeed = req.query.speed || 'auto'; // auto, high, medium, low, quiet
-    const temp = req.query.temp || '72';   // 68, 70, 72...
+    const temp = req.query.temp || '72';   // [68...88] in increments of '2'  
     const command = `${mode}-${fanSpeed}-${temp}F`;
     const output = `Turned on ${mode} mode on ${fanSpeed}, at ${temp} degrees.`;
+    
+    console.log('output: ', output);
 
     if (mode === 'heat') {
         sendCommand(command, function() {
-            console.log(output);
             res.send(output);
         });
 
     } else {
-        sendCommand(`${mode}-on`, function() {
-            console.log('Turned unit on.');
+        console.log('Turning unit on.');
 
+        sendCommand(`${mode}-on`, function() {
             setTimeout(() => {
                 sendCommand(command, function() {
-                    console.log(output);
                     res.send(output);
                 });
             }, 2000);
@@ -56,24 +55,26 @@ app.get('/on', (req, res) => {
 
 // TODO: split into 2 routes -- /alexa/api/v1/set and /set
 app.get('/set', (req, res) => {
-    console.log(req.query);
+    console.log('Changing settings: ', req.query);
 
     const mode = req.query.mode || 'dry'; // cool, heat, dry, fan
     const fanSpeed = req.query.speed || 'auto'; // auto, high, medium, low, quiet
     const temp = req.query.temp || '72';   // 68, 70, 72...
     const command = `${mode}-${fanSpeed}-${temp}F`;
     const output = `Set ${mode} mode on ${fanSpeed}, at ${temp} degrees.`;
+    
+    console.log('output: ', output);
 
     sendCommand(command, function() {
-        console.log(output);
         res.send(output);
     });
 });
 
 // TODO: split into 2 routes -- /alexa/api/v1/off and /off
 app.get('/off', (req, res) => {
+    console.log('Sending power off command.');
+    
     sendCommand('turn-off', function() {
-        console.log('Sent power off command.');
         res.send('The AC is now off.');
     });
 });
